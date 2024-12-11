7581a8731f2cf0a49de87960be68051dcc1fabf2
Alberte Illum <143408303+illumss@users.noreply.github.com>
2024-12-11 15:57:27 +0100

    about section redone

--- frontend/components/5AboutSection.vue
+++ frontend/components/5AboutSection.vue
@@ -1,20 +1,36 @@
 <template>
   <div class="about-section">
-    <h2>Om Muskelmekanikeren</h2>
-    <p>
-      Muskelmekanikeren is a physiotherapy clinic located in Vejle, Denmark. We
-      specialize in pain management, rehabilitation, and personalized
-      treatments.
-    </p>
-    <img src="/images/vægt.png" alt="Vægt" />
+    <h1>Om Muskelmekanikeren</h1>
+    <h2 class="about">
+      Dennis Okholm har været elitesportsudøver størstedelen af sit liv, og ved
+      derfor hvordan det er at have skader og smerter, som begrænser præstation
+      eller bevægelse i dagligdagen.
+    </h2>
+    <img class="about-image" src="/images/weight.png" alt="weight" />
   </div>
 </template>
 
 <style scoped>
 .about-section {
-  padding: 2rem;
-  text-align: center;
+  padding: 5rem;
   background: #233d4d;
   color: white;
+  display: flex;
+  flex-direction: column;
+}
+
+.about {
+  text-align: center;
+  z-index: 2;
+}
+
+.about-image {
+  position: absolute;
+  margin-top: 1rem;
+  width: 100%;
+  max-width: 250px;
+  height: auto;
+  align-self: flex-end;
+  z-index: 1;
 }
 </style>
