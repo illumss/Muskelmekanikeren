b4a3b6cc97cf627824f778f59b0d9b459e1e399a
Alberte Illum <143408303+illumss@users.noreply.github.com>
2024-12-11 17:05:26 +0100

    message redone

--- frontend/components/3MessageSection.vue
+++ frontend/components/3MessageSection.vue
@@ -1,21 +1,34 @@
 <template>
   <div class="message-section">
     <h1>Mission</h1>
-    <h2>
-      Jeg hjælper dig med at overkomme begrænsende smerter og forbedre din
-      præstation.
-    </h2>
-    <img src="/images/fitness-photo.png" alt="Fitness" />
-    <img src="/images/clinic-photo.png" alt="Clinic" />
+    <div class="message-container">
+      <div class="first-box">
+        <h2>
+          Jeg hjælper dig med at overkomme begrænsende smerter og forbedre din
+          præstation.
+        </h2>
+        <img src="/images/fitness-photo.png" alt="Fitness" />
+      </div>
+
+      <div class="second-box">
+        <img src="/images/clinic-photo.png" alt="Clinic" />
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped>
 .message-section {
   background-color: #e0eaef;
-  padding: 2rem;
-  text-align: center;
+  padding: 5rem;
 }
+
+.message-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
 img {
   width: 100%;
   max-width: 600px;
