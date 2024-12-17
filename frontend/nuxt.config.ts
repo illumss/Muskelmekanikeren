9c8cb72a14cd2947b1cb2adc4bad024cb2c9a7e3
Alberte Illum <143408303+illumss@users.noreply.github.com>
2024-12-17 01:06:44 +0100

    build

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,6 +3,12 @@ export default {
     baseURL: "http://localhost:5000/api",
   },
 
+  target: "server",
+  ssr: true,
+  app: {
+    baseURL: "/",
+  },
+
   compatibilityDate: "2024-12-09",
   css: ["@/styles/global.css"],
 };
